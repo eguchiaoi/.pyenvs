88349b829a84df71c78fb3c359b2a480f1f0dccb
eguchiaoi <eguchi@hep.phys.s.u-tokyo.ac.jp>
2021-06-13 17:09:48 +0900

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+cd ~/.pyenvs
+git config --local user.name "eguchiaoi"
+git config --local user.email "eguchi@hep.phys.s.u-tokyo.ac.jp"
+git remote set-url origin git@github.com:eguchiaoi/.pyenvs.git
+
